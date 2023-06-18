3299dea95575b758da62bd7e60d07a251bb2b741
Jeff Cameron <jeffgcameron@gmail.com>
2023-06-18 14:46:52 -0600

    adds path

--- server/server.js
+++ server/server.js
@@ -72,7 +72,8 @@ if (process.env.NODE_ENV === "production") {
     app.get("*", (req, res) => {
 
 
-    res.sendFile(path.resolve(__dirname, "client", "index.html"));
+    res.sendFile(path.resolve("../client/src/index.js"));
+    // res.sendFile(path.resolve(__dirname, "client", "index.html"));
 
    });
 
