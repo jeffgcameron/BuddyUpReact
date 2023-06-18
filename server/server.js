c71142564d8f953850f41013734a607099729c70
Jeff Cameron <jeffgcameron@gmail.com>
2023-06-18 14:40:12 -0600

    adds path

--- server/server.js
+++ server/server.js
@@ -64,13 +64,15 @@ app.get("/", (req, res) => res.send("Api Running"));
 //     });
 //   }
 
+console.log("dirname", __dirname);
 if (process.env.NODE_ENV === "production") {
 
     app.use(express.static("client/build"));
 
     app.get("*", (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+
+    res.sendFile(path.resolve(__dirname, "client", "index.html"));
 
    });
 
