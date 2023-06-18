142644172e5f881963289eb5af8f6bc8ea17a2d1
Jeff Cameron <jeffgcameron@gmail.com>
2023-06-18 15:00:21 -0600

    adds path

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ console.log("dirname", __dirname);
 console.log("process.env", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
 console.log('here inside this');
-    app.use(express.static(__dirname, "../client/build"));
+    app.use(express.static(__dirname + "../client/build"));
 
     app.get("*", (req, res) => {
 
