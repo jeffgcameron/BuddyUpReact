6b73b1ef5e3b4f31c723c6d9adf0564f2c41ed40
Jeff Cameron <jeffgcameron@gmail.com>
2023-06-18 15:12:51 -0600

    adds path

--- server/server.js
+++ server/server.js
@@ -65,11 +65,12 @@ const validateToken = (req, res, next) => {
 //   }
 
 if (process.env.NODE_ENV === "production") {
-
-    // app.use(express.static(__dirname + "../client/public"));
+    
+    app.use(express.static(__dirname + "../client/build"));
 
     app.get("/", (req, res) => {
-        console.log('getting');
+    // app.get("*", (req, res) => {
+        console.log('getting', path.join(__dirname, '../client', 'public', 'index.html'));
         res.sendFile(path.join(__dirname, '../client', 'public', 'index.html'));
 
    });
