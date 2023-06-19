32034b58eb960aa1865abc5a0977217b396c1a57
Jeff Cameron <jeffgcameron@gmail.com>
2023-06-19 15:56:11 -0600

    fixes time and date

--- client/src/components/DateSelector/DateSelector.js
+++ client/src/components/DateSelector/DateSelector.js
@@ -4,47 +4,15 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+const { convertDate }   = require('../../functions/DateFunctions.js');
 
 export default function DateSelector({setDate}) {
 
   var setNewDate = function(data) {
 
-    // var getShortMonth = function() {
-    //     if (data.$M === 1) return 'Jan';
-    //     if (data.$M === 2) return 'Feb';
-    //     if (data.$M === 3) return 'Mar';
-    //     if (data.$M === 4) return 'Apr';
-    //     if (data.$M === 5) return 'May';
-    //     if (data.$M === 6) return 'Jun';
-    //     if (data.$M === 7) return 'Jul';
-    //     if (data.$M === 8) return 'Aug';
-    //     if (data.$M === 9) return 'Sep';
-    //     if (data.$M === 10) return 'Oct';
-    //     if (data.$M === 11) return 'Nov';
-    //    return 'Dec';
-    // }
+    var date = data.$W + '/' + data.$M + '/' + data.$D + '/' +  data.$y
 
-    // var getDayOfWeek = function() {
-    //   if (data.$W === 1) return 'Mon';
-    //   if (data.$W === 1) return 'Tue';
-    //   if (data.$W === 1) return 'Wed';
-    //   if (data.$W === 1) return 'Thu';
-    //   if (data.$W === 1) return 'Fri';
-    //   if (data.$W === 1) return 'Sat';
-    //   return 'Sun'
-    // }
-
-    // var time = {
-    //   month:        data.$M,
-    //   // shortMonth:   getShortMonth(),
-    //   day:          data.$D,
-    //   // dayOfWeek:    getDayOfWeek(),
-    //   year:         data.$y
-    // }
-
-    var time = data.$M + data.$D + data.$y
-
-    setDate(time)
+    setDate(convertDate(date))
 
   }
 
--- client/src/components/TimeSelector/TimeSelector.js
+++ client/src/components/TimeSelector/TimeSelector.js
@@ -4,27 +4,15 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+const { convertTime }   = require('../../functions/DateFunctions.js');
 
 export default function TimeSelector({setTime}) {
-
-  var setNewTime = function(data) {
-    var hour                  = data.$H;
-    var minutes               = data.$m;
-    var minutesPastMidnight   = hour * 60 + minutes;
-    setTime(minutesPastMidnight);
-    // var time = {
-    //   hour:                   hour,
-    //   minutes:                minutes,
-    //   minutesPastMidnight:    minutesPastMidnight
-    // }
-  //  setTime(time);
-  };
 
   return (
     <article className='root-time-selector'>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DemoContainer components={['TimePicker']}>
-            <TimePicker label="Time" onChange={e => setNewTime(e)} />
+            <TimePicker label="Time" onChange={e => setTime(convertTime(e))} />
         </DemoContainer>
         </LocalizationProvider>
     </article>
